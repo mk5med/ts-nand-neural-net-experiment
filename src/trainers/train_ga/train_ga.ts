484f3d5de8a09940be2b2c5d18f910fbca03ec57
local <8334048+mk5med@users.noreply.github.com>
2023-10-09 09:19:11 -0400

    Update train_ga
    - Add isAtLocalMinimumThreshold optional argument.
    - Add bestError initialisation.

--- src/trainers/train_ga/train_ga.ts
+++ src/trainers/train_ga/train_ga.ts
@@ -1,6 +1,6 @@
 import { BinaryNetwork } from "../../network";
 import { TrainData } from "../../types";
-import { generateRandomCoefficients } from "./coefficients";
+import { generateRandomCoefficients } from "./generateCoefficients";
 import { evaluateGeneration } from "./evaluateGeneration";
 import { spawnGeneration } from "./spawnGeneration";
 
@@ -9,6 +9,7 @@ interface TrainArgs {
   population_size?: number;
   maxGenerations?: number;
   error_epsilon?: number;
+  isAtLocalMinimumThreshold?: number;
 }
 
 export async function train({
@@ -16,6 +17,7 @@ export async function train({
   population_size = 10,
   maxGenerations = -1,
   error_epsilon = 0,
+  isAtLocalMinimumThreshold = 10000,
   ...args
 }: TrainArgs) {
   // The total number of connections (coefficients)
@@ -29,11 +31,13 @@ export async function train({
   );
 
   let topPerformers: BinaryNetwork[] = [];
-  let bestError = 1e8;
+  let bestError: number | null = null;
   let count_generations = 0;
   let errorHasNotImprovedCount = 0;
 
   do {
+    const isAtLocalMinimum =
+      errorHasNotImprovedCount == isAtLocalMinimumThreshold;
     const vals = await evaluateGeneration({
       error_epsilon,
       trainData,
@@ -41,8 +45,7 @@ export async function train({
       coefficientCount,
 
       networks,
-      previousBestError: bestError,
-      isAtLocalMinimum: errorHasNotImprovedCount == 10,
+      isAtLocalMinimum,
     });
     console.log(
       `Generation ${count_generations} with error`,
@@ -53,18 +56,23 @@ export async function train({
     topPerformers = vals.topPerformers;
 
     // Reset the count if it has reached the limit
-    if (errorHasNotImprovedCount == 10) {
+    if (isAtLocalMinimum) {
       errorHasNotImprovedCount = 0;
     }
 
+    // Initialise the error if it is unset
+    if (bestError == null) {
+      bestError = vals.newError;
+    }
     // Increment a flag if the error has not improved this round
-    if (bestError <= vals.newError) {
+    else if (bestError <= vals.newError) {
       errorHasNotImprovedCount++;
     }
     // Reset a flag if the error has improved
     else {
       errorHasNotImprovedCount = 0;
       bestError = vals.newError;
+      break;
     }
 
     // Exit if the error of the model is close to the accepted error rate
